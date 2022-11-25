08471efb96faada5a2ed8e70646bbbfd0e7d9245
MMMAAARRRTTT <mctulio.2@gmail.com>
2022-11-24 19:49:07 -0700

    Code folder made
    -main.py created - main config
    -settings.py created -game settings

---
+++ code/main.py
@@ -0,0 +1,23 @@
+import pygame as py
+import sys
+from settings import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.setmode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock = pg.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            pg.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
