afec8c737d115c64b3549ced0bb48fb377b57811
MMMAAARRRTTT <mctulio.2@gmail.com>
2022-11-24 20:32:19 -0700

    level.py created

--- code/main.py
+++ code/main.py
@@ -1,12 +1,15 @@
 import pygame as pg
 import sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        pg.display.set_caption('Test 00')
         self.clock = pg.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -16,6 +19,7 @@ class Game:
                     sys.exit()
 
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pg.display.update()
 
 if __name__ == '__main__':
